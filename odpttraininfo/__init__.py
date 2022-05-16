474946916a7c5f34aa5100bc1de2f09c1218526b
friuli-jokyo <26354730+friuli-jokyo@users.noreply.github.com>
2022-05-16 12:27:10 +0900

    increase version num

--- odpttraininfo/__init__.py
+++ odpttraininfo/__init__.py
@@ -2,6 +2,6 @@ from . import config
 from .cache import fetch_info, refresh_cache
 from .odpt_components import Distributor, TrainInformation, to_json_default
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ["config","fetch_info","refresh_cache","Distributor","TrainInformation","to_json_default"]
